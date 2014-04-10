c076fab279b2ad53630a746f569d45f58efab096
QuigProQuo <johnpeterquigley@gmail.com>
2014-04-10 14:54:14 -0400

    Completed Assignments

--- week_4/my_solutions/4_group_cipher_solution.rb
+++ week_4/my_solutions/4_group_cipher_solution.rb
@@ -1,8 +1,8 @@
 # U2.W4: Cipher Challenge
 
 
-# I worked on this challenge [by myself, with: ].
-
+# I worked on this challenge with 
+# Adeoye Jaiyeola
 
 
 # 1. Solution
@@ -12,14 +12,14 @@
 
 
 def north_korean_cipher(coded_message)
-  input = coded_message.downcase.split("") # Check out this method in IRB to see how it works! Also refer to the ruby docs.
-  decoded_sentence = []
+  input = coded_message.downcase.split("") # this seperates each character in the message, downcases them, and place them into an array..
+  decoded_sentence = [] #this creates an empty array
   cipher = {"e" => "a",   # This is technically a shift of four letters...Can you think of a way to automate this? Is a hash
             "f" => "b",   # the best data structure for this problem? What are the pros and cons of hashes?
-            "g" => "c", 
-            "h" => "d", 
+            "g" => "c",   # Pros: Easy when accounting for 1 shift
+            "h" => "d",   # Cons: If there was more than 1 shift you would have to create a new Hash
             "i" => "e", 
-            "j" => "f",
+            "j" => "f",       
             "k" => "g",
             "l" => "h",
             "m" => "i",
@@ -41,39 +41,70 @@ def north_korean_cipher(coded_message)
             "c" => "y",
             "d" => "z"}
             
-  input.each do |x| # What is #each doing here?
-    found_match = false  # Why would this be assigned to false from the outset? What happens when it's true?
-    cipher.each_key do |y| # What is #each_key doing here?
-      if x == y  # What is this comparing? Where is it getting x? Where is it getting y? What are those variables really?
-        puts "I am comparing x and y. X is #{x} and Y is #{y}."
-        decoded_sentence << cipher[y]
-        found_match = true
-        break  # Why is it breaking here?
-      elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*" #What the heck is this doing?
-        decoded_sentence << " "
-        found_match = true
-        break
-      elsif (0..9).to_a.include?(x) # Try this out in IRB. What does   " (0..9).to_a "    do?
-        decoded_sentence << x
-        found_match = true
-        break
-      end 
-    end
-    if not found_match  # What is this looking for?
-      decoded_sentence << x
-    end
-  end
-  decoded_sentence = decoded_sentence.join("")
+  input.each do |x| # What is #each doing here? #each is allowing us to call each given block once passing the key value pair as a parameter and telling us to do x which at the moment is undefined
+    found_match = false  # Why would this be assigned to false from the outset? What happens when it's true?This line creates a new variable called found_match. False is only a default. As the program keeps running, found_match will change to true if the conditions are met for it to be true.
+    cipher.each_key do |y| # What is #each_key doing here? Calls block once for each key in the hash passing the key as a parameter
+      if x == y  # What is this comparing? Where is it getting x? Where is it getting y? What are those variables really? X is coming from our original input where as Y is coming form our cipher. We are comparing the original input to the cipher.
+        puts "I am comparing x and y. X is #{x} and Y is #{y}." #tells us that 2 letters are being compared.
+        decoded_sentence << cipher[y] #append y to array decoded_sentence
+        found_match = true #automatically changes the  found_match from false to true.
+        break  # Why is it breaking here? Signals that we are moving from if x == y to other possibilities of x
+      elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*" #What the heck is this doing? Best guest.... checking to see if x is equal to any of the strings listed
+        decoded_sentence << " " # adds a space to decoded_sentence if above if true
+        found_match = true #signifies that a match was found
+        break #signifies a break from the above scenario
+      elsif (0..9).to_a.include?(x) # Try this out in IRB. What does   " (0..9).to_a "    do? checking to see if x contains the numbers 0 to 9
+        decoded_sentence << x #adds the number to decoded_sentence if x contains a number
+        found_match = true#signifies that a match was found
+        break#signifies a break from the above scenario
+      end #ends the if-else statements
+    end #ends our cipher.each_key
+    if not found_match  # What is this looking for? # checking to see if no match was found
+      decoded_sentence << x #if no match is found, the input is appended to decode_sentence as it is
+    end  #ends our if not statement
+  end#ends the input.each_key
+  decoded_sentence = decoded_sentence.join("") #joins all elements in the decoded_sentence array with space between each. Turns into string.
  
-  if decoded_sentence.match(/\d+/) #What is this matching? Look at Rubular for help. 
-    decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 } #He's been known to exaggerate...
+  if decoded_sentence.match(/\d+/) #What is this matching? Look at Rubular for help. # checks to see if there are one or more digits in decoded_sentence
+    decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 } #He's been known to exaggerate...#takes the digit, turns it into an integer, and divides by 100
   end  
-  return decoded_sentence # What is this returning?        
+  return decoded_sentence # What is this returning? Our finished product of a decoded North Korean Cipher!        
 end
 
 # Your Refactored Solution
-
-
+def north_korean_cipher(coded_message)
+  input = coded_message.downcase.split("") 
+  decoded_sentence = []
+    letters = ("a".."z").to_a
+  cipher = letters.rotate(-4) 
+    input.each do |x|
+    is_match = false
+    cipher.each_key do |y|
+      if x == y 
+      puts "I am comparing x and y. X is #{x} and Y is #{y}."
+        decoded_sentence << cipher[y] 
+        is_match = true 
+        break  
+      elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*" 
+        decoded_sentence << " " 
+        is_match = true 
+        break 
+      elsif (0..9).to_a.include?(x) 
+        decoded_sentence << x
+        is_match = true
+        break
+      end 
+    end 
+    if not is_match  
+      decoded_sentence << x 
+    end  
+  end
+  decoded_sentence = decoded_sentence.join("") 
+  if decoded_sentence.match(/\d+/) 
+    decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 } 
+  end  
+  return decoded_sentence         
+end
 
 
 
@@ -86,5 +117,5 @@ p north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vi
 p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!")
 p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?")
 
-# Reflection
+ 
  
--- week_4/my_solutions/5_reverse_cipher_solution.rb
+++ week_4/my_solutions/5_reverse_cipher_solution.rb
@@ -10,36 +10,43 @@
 # Also make sure each step is necessary. If you don't think it's necessary
 # Try implementing the code without it. 
 
-def translate_to_cipher(sentence)
-    alphabet = ('a'..'z').to_a
-    cipher = Hash[alphabet.zip(alphabet.rotate(4))]
-    spaces = ["@", "#", "$", "%", "^", "&", "*"]
-    
-    original_sentence = sentence.downcase
-    encoded_sentence = []
-    original_sentence.each_char do |element|
+def translate_to_cipher(sentence) #creates a method, with an array 
+    alphabet = ('a'..'z').to_a # sets alphabet equal to the letters a to z in an array
+    cipher = Hash[alphabet.zip(alphabet.rotate(4))] #creates a hash with the values of the alphabet +4
+    spaces = ["@", "#", "$", "%", "^", "&", "*"] #replaces the spaces with symbols   
+    original_sentence = sentence.downcase #makes the whole sentence downcase
+    encoded_sentence = [] #creates an empty hash
+    original_sentence.each_char do |element| #each character does...
       if cipher.include?(element)
-        encoded_sentence << cipher[element]
+        encoded_sentence << cipher[element] # puts the cipher element into the encoded_sentence
       elsif element == ' '
-        encoded_sentence << spaces.sample
+        encoded_sentence << spaces.sample # puts the spaces samples into the encoded_sentence
       else 
-        encoded_sentence << element
+        encoded_sentence << element #puts the elements into the encoded_sentence
       end
      end
     
-    return encoded_sentence.join
+    return encoded_sentence.join #returns the encoded_sentence decoded!
 end
 
 
 # Questions:
 # 1. What is the .to_a method doing?
+# creating an array of the alphabet
 # 2. How does the rotate method work? What does it work on?
+#it reasigns the value of an array/hash by the specified value.
 # 3. What is `each_char` doing?
+#taking each character and doing a specified task
 # 4. What does `sample` do?
+#picks a sample amount to be tested
 # 5. Are there any other methods you want to understand better?
+#not from this assignment
 # 6. Does this code look better or worse than your refactored solution
+#Better
 #    of the original cipher code? What's better? What's worse?
+#this seems to be more clean cut and precise
 # 7. Is this good code? What makes it good? What makes it bad?
+#I would say this is good code. It is simple, to the point, and organized.
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
@@ -54,4 +61,8 @@ p translate_to_cipher("I want cookies")
 
 
 # 5. Reflection 
+#This was a great assignment to practice variables I don't know
+# If given the opportunity I would like more assignments like this!
+#I felt I learned a lot from just this one simple assignment
+# Especially when it comes to ciphers now. 
 

fe0e61e8b49232b845e37a42ecfe0461d5dd9486
QuigProQuo <johnpeterquigley@gmail.com>
2014-04-15 13:27:46 -0400

    Credit Card Finished

--- week_5/6_validate_credit_card/my_solution.rb
+++ week_5/6_validate_credit_card/my_solution.rb
@@ -4,10 +4,16 @@
 # I worked on this challenge [by myself, with: ].
 
 # 2. Pseudocode
+#create a class for creditcard
+#create a method, used to define the card number
+#have the method raise an arugment when the wrong number is entered
+#create
 
-# Input:
-# Output:
-# Steps:
+
+
+# Input: Credit Card Number
+# Output: Verified number
+
 
 
 # 3. Initial Solution
@@ -15,9 +21,28 @@
 # Don't forget to check on intialization for a card length
 # of exactly 16 digits
 class CreditCard
+	def card_number (numbers)
+		@numbers = numbers
+		raise ArgumentError.new ("Wrong Number") if numbers.to_s.length != 16
+	end # end method card_number
 
-end
+	def verification
+		card = numbers.inspect.split('')
+		x = 0
+		while x < 16
+			card[x] = card[x].to_i * 2
+			x += 2
+	end # end method verification
 
+	card = card.join.split('')
+	total = card.map {|x| x.to_i}.inject(:+)
+	if total % 10 == 0
+		return true
+	else
+		return false
+	end #end if/else
+  end   
+end #end class CreditCard
 
 
 # 4. Refactored Solution
@@ -32,6 +57,10 @@ end
 
 
 
-
-
 # 5. Reflection 
+# This was a test of my knowledge to date. 
+# I defintely struggled with this one! 
+# I feel that this assignment showed me that I need to write better pseudocode
+# because I wasn't quiet sure how to implement my pseudocode once I wrote it.
+# I just ended up trying to solve the problem and then making sure my pseudocode matched (which is wrong)
+# I think I also had trouble because I tried to do this in socrates.

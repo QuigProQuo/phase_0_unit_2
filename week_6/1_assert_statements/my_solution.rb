16088b9d88d43e43d52a129bb0a867ff345b6263
QuigProQuo <johnpeterquigley@gmail.com>
2014-04-20 19:48:19 -0400

    Completed Assignments
    Completed 1,2, & blogs

--- week_6/1_assert_statements/my_solution.rb
+++ week_6/1_assert_statements/my_solution.rb
@@ -1,31 +1,80 @@
 # U2.W6: Testing Assert Statements
 
 # I worked on this challenge [by myself, with: ].
+# Danielle Adams
+# John Quigley
 
 
 # 2. Review the simple assert statement
 
-def assert
-  raise "Assertion failed!" unless yield
-end
+# def assert # create a method called assert, to raise an argument unless it is set to it's tested value
+#   raise "Assertion failed!" unless yield
+# end
 
-name = "bettysue"
-assert { name == "bettysue" }
-assert { name == "billybob" }
+# name = "bettysue"
+# assert { name == "bettysue" }
+# assert { name == "billybob" }
 
 # 2. Pseudocode what happens when the code above runs
+# assert { name == "bettysue" }
+# Define 'assert' method with no arguments
+#   If yield
+#     Return block value
+#   If does not yield
+#     Return 'Assertion failed!' alert
 
+#assert { name == "billybob" }
+#Create a method called assert
+# Raise an argument, test for yield
+# If it doesn't yield, return raised argument
+# If yields, return tested value
 
 
 # 3. Copy your selected challenge here
+# Danielle's code - Week 5, Challenge 4, Part 2
 
+  $boggle_board = [["b", "r", "a", "e"],
+                ["i", "o", "d", "t"],
+                ["e", "c", "l", "r"],
+                ["t", "a", "k", "e"]]
+
+  def get_row(row)
+    $boggle_board[row] # Created global array to use for methods
+  end
+
+  p get_row(1)
+
+  # Driver code #1: Verifies 'get_row' takes 1 argument.
+  # p method(:get_row).arity == 1
+
+  # Driver code #2: Verifies that only listed rows are returned.
+  # p get_row(5) == nil
 
 
 
 # 4. Convert your driver test code from that challenge into Assert Statements
 
+#Driver code #1
+def assert
+  raise "Assertion failed!" unless yield
+end
 
+p argument = 1
+p assert { argument == method(:get_row).arity }
 
+#Driver code #2
+def assert
+  raise "Assertion failed!" unless yield
+end
+
+p row = nil
+p assert { row == get_row(5) }
 
 
 # 5. Reflection
+# Pairing with Danielle for this assignment was a great matchup
+# I felt that we both learned a lot working together
+# Rather then being paired with someone who knew the solution right away bot Danielle and I learned how to do this together.
+# I found ussing assert methods as driver code could be as easy as boolean statements if done correctly
+# This was a great intro challenge for myself to learn how to write driver code in this way
+# I can definetly see myself using this method more often in future challenges!
--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -2,62 +2,67 @@
 
 
 # I worked on this challenge [by myself, with: ].
+# by myself
 
 
 # 2. Original Code
 
 class Drawer
 
-attr_reader :contents
+	attr_reader :contents
 
 # Are there any more methods needed in this class?
+# No
 
-def initialize
-@contents = []
-@open = true
-end
+	def initialize
+		@contents = []
+		@open = true
+	end #end method
 
-def open
-@open = true
-end
+	def open
+		@open = true
+	end #end method
 
-def close
-@open = false
-end 
+	def close
+		@open = false
+	end #end method
 
-def add_item
-@contents << item
-end
+	def add_item(item)
+		@contents << item
+	end #end method
 
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
+	def remove_item(item = @contents.pop) #what is `#pop` doing? #removing the last element from contents or nil in the case there is nothing
+		@contents.delete(item)
+	end #end method
 
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
+	def dump  # what should this method return? #an empty drawer
+		puts "Your drawer is empty."
+	end #end method
 
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
-
+	def view_contents
+		puts "The drawer contains:"
+		@contents.each {|silverware| puts "- " + silverware.type }
+	end #end method
+end #end class
 
 class Silverware
-attr_reader :type
+	attr_reader :type
 
-# Are there any more methods needed in this class?
+# Are there any more methods needed in this class? 
+#yes, method clean_silverware
+	def clean_silverware
+		@clean = true
+	end
+	
+	def initialize(type, clean = true)
+		@type = type
+		@clean = clean
+	end
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
-end
-
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
-
+	def eat
+		puts "eating with the #{type}"
+		@clean = false
+	end
 end
 
 knife1 = Silverware.new("knife")
@@ -83,20 +88,33 @@ removed_knife.clean_silverware
 
 silverware_drawer.view_contents
 silverware_drawer.dump
-silverware_drawer.view_contents #What should this return?
+silverware_drawer.view_contents #What should this return? #the contents of the drawer which should be empty 
 
 
 fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
-fork.eat
+#fork.eat
 
 #BONUS SECTION
-# puts fork.clean
+#puts fork.clean
 
 # DRIVER TESTS GO BELOW THIS LINE
 
 
+drawer = Drawer.new
+fork = Silverware.new("fork")
+drawer.add_item(fork)
+p drawer.remove_item(fork) == fork
+p drawer.dump == []
+p drawer.open == true
+p drawer.close == false
+p fork.eat == false
 
 
 
 
 # 5. Reflection 
+# There are always one or two errors that can trip you up for some extra time when trying to solve them.
+# I found this assignment entertaining to say the least. 
+# Extremely helpful in practicing and understanding the intent of the code someone else wrote. 
+# I found the instructions to be self explanitory (Test in Terminal, fix the bug)
+# Great practice on my own! 

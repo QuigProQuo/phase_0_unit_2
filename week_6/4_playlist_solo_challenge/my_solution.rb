4ac7420f97102a9cbc6acc075ef729360b215be7
QuigProQuo <johnpeterquigley@gmail.com>
2014-04-21 21:29:22 -0400

    Completed Assignments
    Completed Assignment #4

--- week_6/4_playlist_solo_challenge/my_solution.rb
+++ week_6/4_playlist_solo_challenge/my_solution.rb
@@ -2,15 +2,66 @@
 
 
 # I worked on this challenge [by myself, with: ].
-
+# By Myself
 
 # Pseudocode
+=begin 
+	Create a Class, for songs, with 2 variables for artist and title
+	Set these variables equal to themselves, so they can be called upon in methods
+	For songs being played, create a method to put the song being played
 
+	Create another Class to handle the playlist
+	Define methods for initializing, number of tracks, how to add/remove a song,
+	 be sure to pull the song from the array, as well as play all songs
+
+=end
 
 
 # Initial Solution
 
+class Song
+	attr_reader :title, :artist
 
+	def initialize(title, artist)
+		@title = title
+		@artist = artist
+	end
+
+	def play
+		puts "This is #{@title} by #{@artist}"
+	end
+end
+
+class Playlist
+
+	attr_accessor :playlist, :songs
+
+	def initialize(*songs)
+		@playlist = Array.new
+		@playlist += songs
+		
+	end
+
+	def num_of_tracks
+		@playlist.length
+	end
+
+	def add(*songs)
+		@playlist += songs
+	end
+
+	def remove(*songs)
+		@playlist.delete(songs)
+	end
+
+	def includes? songs
+		@playlist.include?(songs)
+	end
+
+	def play_all
+		puts "Playing #{playlist.each}"
+	end
+end
 
 
 # Refactored Solution
@@ -41,5 +92,11 @@ my_playlist.display
 
 
 
-
 # Reflection 
+
+=begin
+	This challenge took me a bit longer than I thought. 
+	I was able to use the driver code to frame my two classes fast and then I had to begin to think what I wanted within those methods.
+	This was great ruby practice.
+
+=end
